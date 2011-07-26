f0c7b22d2ae08954a311b902781e27f9a3a74f8b
Evan Martin <martine@danga.com>
2011-07-26 12:07:24 -0700

    POST only, support optional src/

--- edit-server.py
+++ edit-server.py
@@ -5,40 +5,27 @@ import re
 import subprocess
 import BaseHTTPServer as http
 
-SOURCE_PATH = '/work/chrome/'
+SOURCE_PATHS = ['/work/chrome/', '/work/chrome/src/']
 
 
 def map_path(path):
     if path.startswith('/'):
         path = path[1:]
-    path = os.path.normpath(os.path.join(SOURCE_PATH, path))
-    if not path.startswith(SOURCE_PATH):
-        return None
-    return path
+    for source in SOURCE_PATHS:
+        fullpath = os.path.normpath(os.path.join(source, path))
+        if fullpath.startswith(source) and os.path.exists(fullpath):
+            return fullpath
+    return None
 
 
 class EditHandler(http.BaseHTTPRequestHandler):
-    def verify_path(self):
+    def do_POST(self):
         path = map_path(self.path)
-        if not path or not os.path.exists(path):
+        if not path:
             self.send_response(404)
             self.end_headers()
             return
-        return path
 
-    def do_GET(self):
-        path = self.verify_path()
-        if not path:
-            return
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('%s => %s\n' % (self.path, map_path(self.path)))
-
-    def do_POST(self):
-        path = self.verify_path()
-        if not path:
-            return
         print self.path, path
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
